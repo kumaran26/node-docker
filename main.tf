2e46ff337ed11433afd85ec066e80ad7c29c156d
kumaran <tcshd.kumaran@gmail.com>
2024-09-23 19:59:58 +0530

    fix-8

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "bkt-demo-000"
+  name          = "bkt-demo-00000"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "my-react-project-97ea7"
   force_destroy = true
   public_access_prevention = "enforced"
 }
 
 resource "google_storage_bucket" "my-bucket2" {
-  name          = "bkt-demo-002"
+  name          = "bkt-demo-00222"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "my-react-project-97ea7"
   force_destroy = true
   public_access_prevention = "enforced"
 }
