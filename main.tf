5a0a3d5fd409af91978f76161c03c65d1ef419b8
kumaran <tcshd.kumaran@gmail.com>
2024-09-24 14:20:13 +0530

    change: docker-6

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_storage_bucket" "my-bucket" {
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "my-bucket2" {
+resource "google_storage_bucket" "my-bucket2" { 
   name          = "bkt-demo-00222"
   location      = "us-central1"
   project = "my-react-project-97ea7"
